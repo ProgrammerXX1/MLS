4019c15a04a231f5b7be0ad351ac08ddfb17da4e
bekbolat <b.shanakbay@csi.kz>
2025-07-18 14:45:16 +0500

    V3-34 env

--- backend/app/config/celery_app.py
+++ backend/app/config/celery_app.py
@@ -3,8 +3,8 @@ from celery import Celery
 
 celery_app = Celery(
     "tasks",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0"
 )
 
 celery_app.conf.update(
@@ -13,16 +13,12 @@ celery_app.conf.update(
     result_serializer="json",
     timezone="UTC",
     enable_utc=True,
-    task_routes={"app.tasks.*": {"queue": "default"}},
 )
 
-# Автоматически загружаем задачи из модуля app.tasks
-celery_app.autodiscover_tasks(['app.tasks'])
 celery_app.conf.task_routes = {
     "app.tasks.generate_response_task": {"queue": "default"},
     "app.tasks.generate_response_api_task": {"queue": "api"},
 }
 
-
-# Явно импортируем задачи
+# Явный импорт задач
 import app.tasks

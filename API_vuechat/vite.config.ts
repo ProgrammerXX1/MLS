a2cf99833e9470ad27a4c268efd2c2379dedde11
bekbolat <b.shanakbay@csi.kz>
2025-07-18 17:43:26 +0500

    V3-45 finish

--- API_vuechat/vite.config.ts
+++ API_vuechat/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0',  // 👈 ОБЯЗАТЕЛЬНО для доступа извне контейнера
-    port: 3001        // не обязательно, но можно указать явно
+    host: '0.0.0.0', // обязательно для доступа извне
+    port: 5173       // это порт Vite внутри контейнера
   }
 })

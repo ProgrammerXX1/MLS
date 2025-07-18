d2d291ab0b13ac91fc1c7f2a063fe12420693bac
bekbolat <b.shanakbay@csi.kz>
2025-07-18 17:32:17 +0500

    V3-45 finish

--- API_vuechat/vite.config.ts
+++ API_vuechat/vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',  // 👈 ОБЯЗАТЕЛЬНО для доступа извне контейнера
+    port: 5173        // не обязательно, но можно указать явно
+  }
 })
